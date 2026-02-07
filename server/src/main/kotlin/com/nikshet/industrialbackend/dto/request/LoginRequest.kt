67aab90e71489b7b11f11cd5acc30931bf88b887
NikshetSteh <stehnikshet@gmail.com>
2026-02-07 21:49:51 +0500

    fix: Small fix

--- server/src/main/kotlin/com/nikshet/industrialbackend/dto/request/LoginRequest.kt
+++ server/src/main/kotlin/com/nikshet/industrialbackend/dto/request/LoginRequest.kt
@@ -3,9 +3,9 @@ package com.nikshet.industrialbackend.dto.request
 import jakarta.validation.constraints.NotBlank
 
 data class LoginRequest(
-    @field:NotBlank(message = "Phone number is required")
+    @field:NotBlank(message = "Требуется указать номер телефона")
     val phoneNumber: String,
 
-    @field:NotBlank(message = "Password is required")
+    @field:NotBlank(message = "Требуется указать пароль")
     val password: String
 )
